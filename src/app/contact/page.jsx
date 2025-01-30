00216a676a2242be293b04d75733cac6eb4c7dbc
longsreyly <longsreyly85@gmail.com>
2025-01-30 15:24:39 +0700

    delete input name

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -48,7 +48,7 @@ export default function ContactForm() {
                     style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }}>
                 </iframe>
             </div>
-            <div className="container px-5 py-24 mx-auto flex">
+            <div className="container px-5 py-24 mx-auto flex" data-aos="zoom-in-left" >
                 <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
                     <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
                     <small className="leading-relaxed mb-5 text-gray-600">
