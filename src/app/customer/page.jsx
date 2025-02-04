c9c28979a6a4b0c61560a680a1b0431a12bc1985
longsreyly <longsreyly85@gmail.com>
2025-02-04 08:55:36 +0700

    clean code

--- src/app/customer/page.jsx
+++ src/app/customer/page.jsx
@@ -1,55 +1,81 @@
-"use client"
-import { getAllCustomerService, insertCustomerService, updateCustomerService } from "@/service/customer.service";
+"use client";
+import {
+  getAllCustomerService,
+  insertCustomerService,
+  updateCustomerService,
+} from "@/service/customer.service";
 import { useEffect, useState } from "react";
 import FormComponent from "./_component/FormComponent";
 import TableComponent from "@/app/customer/_component/TableComponent";
 import { deleteCustomerServiceAction } from "../action/customerAction";
 
 const CustomerComponent = () => {
-    const [customerData, setCustomerData] = useState([]); // State for customer data
-    const [editingCustomer, setEditingCustomer] = useState(null); // State for editing a customer
-    const [selectedCustomer, setSelectedCustomer] = useState(null);
+  const [customerData, setCustomerData] = useState([]); // State for customer data
+  const [editingCustomer, setEditingCustomer] = useState(null); // State for editing a customer
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
 
-    // Example of setting a default value:
-    useEffect(() => {if (!selectedCustomer) { setSelectedCustomer({ customerName: "", gender: "", address: "" });}},[selectedCustomer]);
-    // Fetch customer data on component mount
-    useEffect(() => {
-        const fetchData = async () => {
-        const data = await getAllCustomerService();
-        setCustomerData(data);
-    };fetchData();},[]);
-
-    // Handle form submission for adding/updating customers
-    const handleFormSubmit = async (data) => {
-        if (editingCustomer) {
-            const updatedCustomer = await updateCustomerService(editingCustomer.id, data);
-            setCustomerData((prev) =>
-                prev.map((customer) => (customer.id === editingCustomer.id ? updatedCustomer : customer))
-            );   
-            setEditingCustomer(null); //Reset editing state to trigger form reset
-        } else {
-            const newCustomer = await insertCustomerService(data);
-            setCustomerData((prev) => [...prev, newCustomer]);
-        }
+  // Example of setting a default value:
+  useEffect(() => {
+    if (!selectedCustomer) {
+      setSelectedCustomer({ customerName: "", gender: "", address: "" });
+    }
+  }, [selectedCustomer]);
+  // Fetch customer data on component mount
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getAllCustomerService();
+      setCustomerData(data);
     };
-    
-    // Handle customer edit
-    const handleEditCustomer = (customer) => {setEditingCustomer(customer);};// Set the customer to edit
-    // Handle customer delete
-    const handleDeleteCustomer = async (customerId) => {
-        const updatedData = await deleteCustomerServiceAction(customerId); setCustomerData(updatedData); }; // Update the state with the latest customer data
-    return (
-        <main className="mx-10">
-            <div className="grid grid-cols-2 d-flex" >
-                <div>
-                    <FormComponent customer={editingCustomer} onFormSubmit={handleFormSubmit} />
-                </div>
-                <div>
-                    <h1 className="mb-[35px]"></h1>
-                    <TableComponent customerData={customerData} onEditCustomer={handleEditCustomer} onDeleteCustomer={handleDeleteCustomer} />
-                </div>
-            </div>
-        </main>     
-    );
+    fetchData();
+  }, []);
+
+  // Handle form submission for adding/updating customers
+  const handleFormSubmit = async (data) => {
+    if (editingCustomer) {
+      const updatedCustomer = await updateCustomerService(
+        editingCustomer.id,
+        data
+      );
+      setCustomerData((prev) =>
+        prev.map((customer) =>
+          customer.id === editingCustomer.id ? updatedCustomer : customer
+        )
+      );
+      setEditingCustomer(null); //Reset editing state to trigger form reset
+    } else {
+      const newCustomer = await insertCustomerService(data);
+      setCustomerData((prev) => [...prev, newCustomer]);
+    }
+  };
+
+  // Handle customer edit
+  const handleEditCustomer = (customer) => {
+    setEditingCustomer(customer);
+  }; // Set the customer to edit
+  // Handle customer delete
+  const handleDeleteCustomer = async (customerId) => {
+    const updatedData = await deleteCustomerServiceAction(customerId);
+    setCustomerData(updatedData);
+  }; // Update the state with the latest customer data
+  return (
+    <main className="mx-10">
+      <div className="grid grid-cols-2 d-flex">
+        <div>
+          <FormComponent
+            customer={editingCustomer}
+            onFormSubmit={handleFormSubmit}
+          />
+        </div>
+        <div>
+          <h1 className="mb-[35px]"></h1>
+          <TableComponent
+            customerData={customerData}
+            onEditCustomer={handleEditCustomer}
+            onDeleteCustomer={handleDeleteCustomer}
+          />
+        </div>
+      </div>
+    </main>
+  );
 };
 export default CustomerComponent;
